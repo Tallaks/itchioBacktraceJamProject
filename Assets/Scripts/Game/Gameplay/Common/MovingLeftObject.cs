9593302ac900c9107cf61442270e116ce5b0bb92
Tallaks <sibirtsev@phystech.edu>
2022-06-25 05:10:21 +0400

    Added component for moving objects (backgroud, obstacles) to the left

---
+++ Assets/Scripts/Game/Gameplay/Common/MovingLeftObject.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace Game.Gameplay.Common
+{
+  public class MovingLeftObject : MonoBehaviour
+  {
+    [SerializeField, Range(1, 100)] 
+    private float _speed;
+
+    private void Update() => 
+      transform.Translate(Vector3.left * _speed * Time.deltaTime);
+  }
+}
